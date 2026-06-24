62b424798ed9ce6107bbdbd8de3422ed58261011
RicardoNS <jricardo.navarros@gmail.com>
2026-06-23 21:58:42 -0600

    Ultimos retoques a Tailwind

--- ejercicios_css/tailwind/assets/js/index.js
+++ ejercicios_css/tailwind/assets/js/index.js
@@ -1,13 +1,9 @@
 const d = document;
 const $btnHamburger = d.getElementById("btn-menu");
-const $iconHamburger = d.querySelector("#btn-menu .svg-hamburguer");
-const $iconX = d.querySelector("#btn-menu .svg-x");
 const $blockMenu = d.getElementById("block-menu");
 
 
 $btnHamburger.addEventListener("click", () => {
-  $iconHamburger.classList.toggle("hidden");
-  $iconX.classList.toggle("hidden");
   $blockMenu.classList.toggle("hidden");
   $blockMenu.classList.toggle("flex");
 });
